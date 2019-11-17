47556f39b532dc8e7b977b3df1f9fb59313a9a03
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-11-17 21:30:30 +1100

    Fix newlines in test asserts

--- src/test/java/com/danclowry/noteapp/markdown/MarkdownParserTest.java
+++ src/test/java/com/danclowry/noteapp/markdown/MarkdownParserTest.java
@@ -8,7 +8,7 @@ public class MarkdownParserTest {
     @Test
     public void parseToHTML_inputNormalText_returnsParagraphInHTMLBody() {
         MarkdownParser mdParser = new MarkdownParser();
-        assertEquals("<!DOCTYPE html><body><p>body text</p></body>",
+        assertEquals("<!DOCTYPE html><body><p>body text</p>\n</body>",
                 mdParser.parseToHTML("body text"),
                 "Input text should be placed within a paragraph <p> element");
     }
@@ -16,7 +16,7 @@ public class MarkdownParserTest {
     @Test
     public void parseToHTML_inputLevelOneHeading_returnsH1InHTMLBody() {
         MarkdownParser mdParser = new MarkdownParser();
-        assertEquals("<!DOCTYPE html><body><h1>level one heading</h1></body>",
+        assertEquals("<!DOCTYPE html><body><h1>level one heading</h1>\n</body>",
                 mdParser.parseToHTML("# level one heading"),
                 "Input text should be placed within a level-one heading <h1> element");
     }
