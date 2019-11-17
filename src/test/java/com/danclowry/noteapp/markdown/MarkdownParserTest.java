43b1bb8b5741210a05ac66d6ccbc581707642689
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-11-17 19:59:12 +1100

    Add test for parsing Markdown lvl 1 heading to HTML

--- src/test/java/com/danclowry/noteapp/markdown/MarkdownParserTest.java
+++ src/test/java/com/danclowry/noteapp/markdown/MarkdownParserTest.java
@@ -12,4 +12,12 @@ public class MarkdownParserTest {
                 mdParser.parseToHTML("body text"),
                 "Input text should be placed within a paragraph <p> element");
     }
+
+    @Test
+    public void parseToHTML_inputLevelOneHeading_returnsH1InHTMLBody() {
+        MarkdownParser mdParser = new MarkdownParser();
+        assertEquals("<!DOCTYPE html><body><h1>level one heading</h1></body>",
+                mdParser.parseToHTML("# level one heading"),
+                "Input text should be placed within a level-one heading <h1> element");
+    }
 }
