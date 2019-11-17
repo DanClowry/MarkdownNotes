e3f2ebf9aff0b3513aac628aa8d2cb205d1fab78
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-11-17 21:24:49 +1100

    Change parse method to use flexmark library

--- src/main/java/com/danclowry/noteapp/markdown/MarkdownParser.java
+++ src/main/java/com/danclowry/noteapp/markdown/MarkdownParser.java
@@ -1,18 +1,17 @@
 package com.danclowry.noteapp.markdown;
 
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+
 public class MarkdownParser {
 
+    private Parser parser = Parser.builder().build();
+    private HtmlRenderer renderer = HtmlRenderer.builder().build();
     private String htmlTemplate = "<!DOCTYPE html><body>PLACEHOLDER</body>";
 
     public String parseToHTML(String input) {
-        String htmlBody;
-
-        if (input.startsWith("#")) {
-            htmlBody = "<h1>" + input.substring(2) + "</h1>";
-            return htmlTemplate.replace("PLACEHOLDER", htmlBody);
-        }
-
-        htmlBody = "<p>" + input + "</p>";
-        return htmlTemplate.replace("PLACEHOLDER", htmlBody);
+        String mdHTML = renderer.render(parser.parse(input));
+        return htmlTemplate.replace("PLACEHOLDER", mdHTML);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.danclowry.noteapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires flexmark;
+    requires flexmark.util;
 
     opens com.danclowry.noteapp to javafx.fxml;
     exports com.danclowry.noteapp;
