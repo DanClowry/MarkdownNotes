c5cef251334359dd51cee75164cd310d38854396
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-11-19 21:00:47 +1100

    Add database settings view and menubar item

--- src/main/java/com/danclowry/noteapp/controllers/EditorController.java
+++ src/main/java/com/danclowry/noteapp/controllers/EditorController.java
@@ -1,15 +1,25 @@
 package com.danclowry.noteapp.controllers;
 
 import com.danclowry.noteapp.markdown.MarkdownParser;
+import com.danclowry.noteapp.util.LoadFXML;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.web.WebView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class EditorController {
     @FXML
     private TextArea markdownEditor;
     @FXML
     private WebView markdownViewer;
+    @FXML
+    private MenuItem settingsMenuItem;
 
     public void initialize() {
         MarkdownParser mdParser = new MarkdownParser();
@@ -18,5 +28,20 @@ public class EditorController {
 
         markdownEditor.textProperty().addListener((observable, oldValue, newValue) ->
                 markdownViewer.getEngine().loadContent(mdParser.parseToHTML(markdownEditor.getText()), "text/html"));
+
+        settingsMenuItem.setOnAction(e -> {
+            try {
+                Stage settings = new Stage();
+                settings.setScene(new Scene(LoadFXML.loadFXML("settings")));
+                settings.initModality(Modality.APPLICATION_MODAL);
+                settings.setResizable(false);
+                settings.showAndWait();
+            } catch (IOException ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setContentText("Error opening settings menu");
+                alert.showAndWait();
+            }
+        });
     }
 }
