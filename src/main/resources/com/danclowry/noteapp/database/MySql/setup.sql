c38a643ff38c1fa18625699ba899199301917284
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-11-22 12:13:06 +1100

    Add MySQL database setup script

---
+++ src/main/resources/com/danclowry/noteapp/database/MySql/setup.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE IF NOT EXISTS Notes DEFAULT CHARACTER SET utf8mb4;
+USE Notes;
+
+CREATE TABLE IF NOT EXISTS Notes.Note (
+    NoteID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(100) NOT NULL,
+    Content MEDIUMTEXT CHARACTER SET utf8mb4
+);
+
+DROP PROCEDURE IF EXISTS Note_Create;
+CREATE PROCEDURE Note_Create
+(
+    IN title VARCHAR(100),
+    IN content MEDIUMTEXT
+)
+BEGIN
+    INSERT INTO Note
+    (
+        Title,
+        Content
+    )
+    VALUES
+    (
+        title,
+        content
+    );
+END;
+
+DROP PROCEDURE IF EXISTS Note_Update;
+CREATE PROCEDURE Note_Update
+(
+    IN id INT,
+    IN title VARCHAR(100),
+    IN content MEDIUMTEXT
+)
+BEGIN
+    UPDATE Note
+    SET Title = title, Content = content
+    WHERE NoteID = id;
+END;
+
+DROP PROCEDURE IF EXISTS Note_Delete;
+CREATE PROCEDURE Note_Delete
+(
+    IN id INT
+)
+BEGIN
+    DELETE FROM Note
+    WHERE NoteID = id;
+END;
+
+DROP PROCEDURE IF EXISTS Note_SelectAll;
+CREATE PROCEDURE Note_SelectAll()
+BEGIN
+    SELECT * FROM Note;
+END;
+
+DROP PROCEDURE IF EXISTS Note_SelectByID;
+CREATE PROCEDURE Note_SelectByID
+(
+    IN id INT
+)
+BEGIN
+    SELECT * FROM Note WHERE NoteID = id;
+END;
