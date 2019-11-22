1716fc5740e5636b6657633d88dcf1fd2eb87e82
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-11-22 18:20:41 +1100

    Add note model constructors

--- src/main/java/com/danclowry/noteapp/models/Note.java
+++ src/main/java/com/danclowry/noteapp/models/Note.java
@@ -5,6 +5,17 @@ public class Note {
     private String title;
     private String content;
 
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Note(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
     public int getId() {
         return id;
     }
