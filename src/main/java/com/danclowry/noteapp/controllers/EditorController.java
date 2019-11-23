51a88f0eaa954f193344c936c6ac8d0314240f50
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-11-23 18:29:41 +1100

    Add function to populate editor notes list from DB

--- src/main/java/com/danclowry/noteapp/controllers/EditorController.java
+++ src/main/java/com/danclowry/noteapp/controllers/EditorController.java
@@ -1,18 +1,22 @@
 package com.danclowry.noteapp.controllers;
 
+import com.danclowry.noteapp.database.Repository;
+import com.danclowry.noteapp.database.mysql.MySqlRepository;
 import com.danclowry.noteapp.markdown.MarkdownParser;
+import com.danclowry.noteapp.models.Note;
 import com.danclowry.noteapp.util.AlertBuilder;
 import com.danclowry.noteapp.util.LoadFXML;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.web.WebView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class EditorController {
     @FXML
@@ -21,6 +25,8 @@ public class EditorController {
     private WebView markdownViewer;
     @FXML
     private MenuItem settingsMenuItem;
+    @FXML
+    private ListView notesListView;
 
     public void initialize() {
         MarkdownParser mdParser = new MarkdownParser();
@@ -55,5 +61,32 @@ public class EditorController {
                 alert.showAndWait();
             }
         });
+
+        // Set cells to show title of note and wrap text
+        notesListView.setCellFactory(p -> new ListCell<Note>() {
+            @Override
+            protected void updateItem(Note note, boolean empty) {
+                super.updateItem(note, empty);
+
+                if (empty || note == null || note.getTitle() == null) {
+                    setText(null);
+                } else {
+                    setText(note.getTitle());
+                    setWrapText(true);
+                    setPrefWidth(notesListView.getWidth() - 2.0);
+                }
+            }
+        });
+
+        try {
+            Repository repository = new MySqlRepository();
+            ObservableList<Note> notesList = FXCollections.observableList(repository.getListOfNotes());
+            notesListView.setItems(notesList);
+        } catch (SQLException ex) {
+            Alert alert = AlertBuilder.createExceptionAlert("Error- Failed to load saved notes",
+                    "Could not load saved notes from the database.\n" +
+                            "Please check your database connection settings", ex);
+            alert.showAndWait();
+        }
     }
 }
