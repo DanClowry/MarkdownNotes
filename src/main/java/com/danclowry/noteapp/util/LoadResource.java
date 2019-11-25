014409f846847098711fdd37144483fb492a345e
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-11-25 19:08:41 +1100

    Change resource loader to preserve line breaks

--- src/main/java/com/danclowry/noteapp/util/LoadResource.java
+++ src/main/java/com/danclowry/noteapp/util/LoadResource.java
@@ -2,18 +2,23 @@ package com.danclowry.noteapp.util;
 
 import com.danclowry.noteapp.App;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.LineNumberReader;
 import java.net.URISyntaxException;
-import java.util.stream.Collectors;
+import java.util.StringJoiner;
 
 public class LoadResource {
     public static String LoadResource(String path) throws IOException, URISyntaxException {
         File file = new File(App.class.getResource(path).toURI());
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            return br.lines().collect(Collectors.joining());
+        try (LineNumberReader lr = new LineNumberReader(new FileReader(file))) {
+            StringJoiner sr = new StringJoiner(System.getProperty("line.separator"));
+            String line;
+            while ((line = lr.readLine()) != null) {
+                sr.add(line);
+            }
+            return sr.toString();
         }
     }
 }
